8b30fc64258c4bcb8be390a028cccd8f26e1ff79
Pedro Resende <55058094+ToDDiNhOBR@users.noreply.github.com>
2025-01-29 07:50:11 -0300

    Refino de design

--- script.js
+++ script.js
@@ -26,14 +26,19 @@ function createTaskElement(task) {
     taskDiv.innerHTML = `
         <div class="task-content">
             <span class="${task.completed ? 'completed' : ''}">${task.name}</span>
-            <span class="date">${task.label} - Criado em: ${new Date().toLocaleDateString()}</span>
-            ${!task.completed ? '<button class="completeBtn">Concluir</button>' : ''}
-            <span class="checkIcon" style="display: ${task.completed ? 'flex' : 'none'};">
-                <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="16" cy="16.5" r="16" fill="#00D8A7"/>
-                    <path d="M10.6667 17.1666L14 20.5L21.3334 13.1666" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </span>
+            <div class="task-details">
+                <div class="label">${task.label}</div>
+                <div class="date">Criado em: ${new Date().toLocaleDateString()}</div>
+            </div>
+            <div class="task-actions">
+                ${!task.completed ? '<button class="completeBtn">Concluir</button>' : ''}
+                <span class="checkIcon" style="display: ${task.completed ? 'flex' : 'none'};">
+                    <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="16" cy="16.5" r="16" fill="#00D8A7"/>
+                        <path d="M10.6667 17.1666L14 20.5L21.3334 13.1666" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </span>
+            </div>
         </div>
     `;
 
